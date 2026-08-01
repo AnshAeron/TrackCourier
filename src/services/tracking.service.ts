7b678f6be18a8cb6301a462b693e7fb53341ee51
ANSH <ansh@ANSHs-MacBook-Air.local>
2026-08-01 22:13:27 +0530

    New changes

--- src/services/tracking.service.ts
+++ src/services/tracking.service.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "https://trackcourier-jut7.onrender.com/api/track";
+const API = "https://trackcourier-jut7.onrender.com";
 
 export const getShipment = async (trackingId: string) => {
   const response = await axios.get(`${API}/${trackingId}`);
