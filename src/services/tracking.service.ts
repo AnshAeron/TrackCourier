0c5c5e578ddbc375a5466548b1c7d79f0288c9cc
ANSH <ansh@ANSHs-MacBook-Air.local>
2026-08-01 22:15:20 +0530

    Connect frontend to Render backend

--- src/services/tracking.service.ts
+++ src/services/tracking.service.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "https://trackcourier-jut7.onrender.com";
+const API = "https://trackcourier-jut7.onrender.com/api/track";
 
 export const getShipment = async (trackingId: string) => {
   const response = await axios.get(`${API}/${trackingId}`);
