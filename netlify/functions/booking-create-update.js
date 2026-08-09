2368e2b003424ead54bea917bb9832e095d4179a
anujbehal <68441127+anujbehal@users.noreply.github.com>
2026-08-09 13:08:55 +0530

    Set default value for provider_tracking_id

--- netlify/functions/booking-create-update.js
+++ netlify/functions/booking-create-update.js
@@ -69,6 +69,9 @@ export async function handler(event) {
       });
     }
 
+    if (!provider_tracking_id) {
+      provider_tracking_id = "NA";
+    }
     const consignmentA = String(internal_tracking_id).trim();
 
     client = await pool.connect();
