cc347aeb28f560b54ca8d54ab70f6b22d38fccfd
ANSH <ansh@ANSHs-MacBook-Air.local>
2026-09-09 00:07:24 +0530

    Changes

--- src/pages/Tracking.tsx
+++ src/pages/Tracking.tsx
@@ -6,7 +6,7 @@ const carrierLogos: Record<string, string> = {
   Delhivery: "/logos/delhivery.png",
   ABCStar: "/logos/abcstar.png",
   M5C: "/logos/m5c.png",
-  "Dilli King": "/logos/dilliking.png",
+  DilliKing: "/logos/dilliking.png",
 };
 
 import { useEffect, useState } from "react";
